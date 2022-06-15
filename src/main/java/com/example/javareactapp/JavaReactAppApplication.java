9a961ca6756bd7b99fda6cf48899e284c0c72653
Maryam Keshavarz <k_maryam27@yahoo.com>
2022-06-15 15:44:00 -0500

    test version

---
+++ src/main/java/com/example/javareactapp/JavaReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javareactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javareactapp/JavaReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javareactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
